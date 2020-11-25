fe72c02905818fca1723fd1d45be63ea8218d683
Jpendy <Jpendy256@gmail.com>
2020-11-24 22:12:15 -0800

    delete

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -7,34 +7,37 @@ export default function PostsList({ posts, handleVote }) {
 
     const activeUser = useActiveUser()
 
-    const list = posts.map((item, i) => {
-        return (
-            <li key={i} className='list-item' >
-                <Link to={`/post-details/${item.id}`} >
-                    <h2 className='post-title' >{item.title}</h2>
-                </Link>
-                {activeUser && <button
-                    className='vote-button'
-                    onClick={(e) => handleVote(item.id, e)}
-                    value={1} >
-                    Like
+    const list = posts
+        .sort((a, b) => b.vote_score - a.vote_score)
+        .map((item, i) => {
+            return (
+                <li key={i} className='list-item' >
+                    <Link to={`/post-details/${item.id}`} >
+                        <h2 className='post-title' >{item.title}</h2>
+                    </Link>
+                    {activeUser && <button
+                        className='vote-button'
+                        onClick={(e) => handleVote(item.id, e)}
+                        value={1} >
+                        Like
                 </button>}
 
-                <span className='vote-score' > {item.vote_score} </span>
+                    <span className='vote-score' > {item.vote_score} </span>
 
-                {activeUser && <button
-                    className='vote-button'
-                    onClick={(e) => handleVote(item.id, e)}
-                    value={-1}
-                >Dislike
+                    {activeUser && <button
+                        className='vote-button'
+                        onClick={(e) => handleVote(item.id, e)}
+                        value={-1}
+                    >Dislike
                  </button>}
 
-                <p className='category' >{item.category}</p>
-                <img className='image' src={item.image || 'https://placekitten.com/200/300'} style={{ width: '150px' }} alt='' />
-                {item.video && <iframe className='video' title='video' src={item.video} />}
-            </li>
-        )
-    })
+                    <p className='category' >{item.category}</p>
+                    <img className='image' src={item.image || 'https://placekitten.com/200/300'} style={{ width: '150px' }} alt='' />
+                    {item.video && <iframe className='video' title='video' src={item.video} />}
+                </li>
+            )
+        })
+
 
     return (
         <ul className='post-list' >
