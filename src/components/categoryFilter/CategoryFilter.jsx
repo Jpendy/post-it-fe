9307eef1be0532f4c17f149cac6dddc2b6569fcb
Jpendy <Jpendy256@gmail.com>
2020-11-28 13:13:07 -0800

    distinct categories

--- src/components/categoryFilter/CategoryFilter.jsx
+++ src/components/categoryFilter/CategoryFilter.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 export default function CategoryFilter({ posts, handleFilterChange }) {
 
     const categories = [...new Set(posts.map(item => item.category))]
+
+
     return (
-        <div>
-            <select onChange={handleFilterChange} >
-                <option value='' >All Categories</option>
-                {categories.map((category, i) => <option key={i} value={category} >{category}</option>)}
-            </select>
-        </div>
+        <>
+            <div>
+
+            </div>
+
+
+            <div>
+                <select onChange={handleFilterChange} >
+                    <option value='' >All Categories</option>
+                    {categories.map((category, i) => <option key={i} value={category} >{category}</option>)}
+                </select>
+            </div>
+        </>
     )
 }
--- src/containers/CreatePost/CreatePost.jsx
+++ src/containers/CreatePost/CreatePost.jsx
@@ -34,30 +34,42 @@ export default function CreatePost() {
     }
 
     return (
-        <div className='create-post-form-area' >
-            <h3>Create Post</h3>
+        <>
+            <div className='create-post-form-area' >
+                <h3>Create Post</h3>
 
-            <form onSubmit={handleSubmit} className='create-post-form' >
+                <form onSubmit={handleSubmit} className='create-post-form' >
 
-                <input className='create-post-form-input' placeholder='title' onChange={e => setTitle(e.target.value)} />
+                    <input className='create-post-form-input' placeholder='title' onChange={e => setTitle(e.target.value)} />
 
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    {
-                        createCategory
-                            ? <input ref={categoryInput} style={{ width: '200px' }} className='create-post-form-input' placeholder='category' onChange={e => setCategory(e.target.value)} />
-                            : <select style={{ width: '387px' }} className='create-post-form-input' onChange={e => setCategory(e.target.value)}>
-                                <option value='none' >choose category</option>
-                                {posts.map((item, i) => <option key={i} >{item.category}</option>)}
-                            </select>
-                    }
-                    <span><button style={{ height: '25px', cursor: 'pointer' }} type='button' onClick={handleClick} >{createCategory ? 'Choose Category' : 'Add Category'}</button></span>
-                </div>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        {
+                            createCategory
+                                ? <input ref={categoryInput} style={{ width: '200px' }} className='create-post-form-input' placeholder='category' onChange={e => setCategory(e.target.value)} />
+                                : <select style={{ width: '387px' }} className='create-post-form-input' onChange={e => setCategory(e.target.value)}>
+                                    <option value='none' >choose category</option>
+                                    {posts.map((item, i) => <option key={i} >{item.category}</option>)}
+                                </select>
+                        }
+                        <span><button style={{ height: '25px', cursor: 'pointer' }} type='button' onClick={handleClick} >{createCategory ? 'Choose Category' : 'Add Category'}</button></span>
+                    </div>
 
-                <input className='create-post-form-input' placeholder='image url - optional' style={{ display: 'block' }} onChange={e => setImage(e.target.value)} />
-                <input className='create-post-form-input' placeholder='video url - optional' style={{ display: 'block' }} onChange={e => setVideo(e.target.value)} />
-                <textarea className='create-post-form-textarea' placeholder='text body' style={{ display: 'block' }} onChange={e => setBody(e.target.value)} />
-                <button className='create-post-button' >Submit</button>
-            </form>
-        </div>
+                    <input className='create-post-form-input' placeholder='image url - optional' style={{ display: 'block' }} onChange={e => setImage(e.target.value)} />
+                    <input className='create-post-form-input' placeholder='video url - optional' style={{ display: 'block' }} onChange={e => setVideo(e.target.value)} />
+                    <textarea className='create-post-form-textarea' placeholder='text body' style={{ display: 'block' }} onChange={e => setBody(e.target.value)} />
+                    <button className='create-post-button' >Submit</button>
+                </form>
+            </div>
+
+
+            {(title || category || image || video || body) &&
+                <div className="create-post-preview-area" >
+                    {title && <h3>Title: {title}</h3>}
+                    {category && <p>Category: {category}</p>}
+                    {image && <img src={image} alt='' />}
+                    {video && <iframe src={video} title='title' />}
+                    {body && <p>{body}</p>}
+                </div>}
+        </>
     )
 }
