253962071cf87ecd1a8af339f202725e91ea3f9a
Jpendy <Jpendy256@gmail.com>
2020-11-28 12:10:27 -0800

    distinct categories

--- src/components/categoryFilter/CategoryFilter.jsx
+++ src/components/categoryFilter/CategoryFilter.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default function CategoryFilter({ posts, handleFilterChange }) {
+
+    const categories = [...new Set(posts.map(item => item.category))]
     return (
         <div>
             <select onChange={handleFilterChange} >
                 <option value='' >All Categories</option>
-                {posts.map(({ category }, i) => <option key={i} value={category} >{category}</option>)}
+                {categories.map((category, i) => <option key={i} value={category} >{category}</option>)}
             </select>
         </div>
     )
