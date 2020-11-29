80fc484dcfca7a60b51a8528e879d5910d6d2347
Jpendy <Jpendy256@gmail.com>
2020-11-29 03:19:01 -0800

    commit

--- src/containers/Signup/Signup.jsx
+++ src/containers/Signup/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useSignup, useAuthError } from '../../hooks/AuthContext'
+import './Signup.css'
 
 export default function Signup() {
     const signup = useSignup();
@@ -14,11 +15,12 @@ export default function Signup() {
     }
 
     return (
-        <div>
+        <div className='sign-up-area' >
+            <img src='/postr-logo2.png' alt='' />
             <form onSubmit={handleSubmit} >
                 {error && <p style={{ color: 'red' }} >{`${error.error}`}</p>}
                 <input placeholder='username' onChange={({ target }) => setEmail(target.value)} />
-                <input placeholder='password' onChange={({ target }) => setPassword(target.value)} />
+                <input type='password' placeholder='password' onChange={({ target }) => setPassword(target.value)} />
                 <button>submit</button>
             </form>
         </div>
